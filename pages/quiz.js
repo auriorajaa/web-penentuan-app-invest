de7cb70a29b9f6a884ec8d721aa91f1cf2976889
Aurio Rajaa <riorajaa2018@gmail.com>
2024-07-22 23:56:46 +0700

    fix how to page

--- pages/quiz.js
+++ pages/quiz.js
@@ -183,6 +183,7 @@ document.addEventListener('DOMContentLoaded', () => {
       questions.forEach((question, index) => {
          const weight = userWeights[index];
          if (question.isCost) {
+            // Normalisasi terbalik untuk kriteria biaya
             const maxScore = Math.max(...Object.values(question.scores));
             Object.keys(question.scores).forEach(app => {
                totalScores[app] += ((maxScore - question.scores[app]) / maxScore) * weight;
@@ -224,107 +225,110 @@ document.addEventListener('DOMContentLoaded', () => {
 
          const newWindow = window.open('', '_blank');
          newWindow.document.write(`
-                <html>
-                <head>
-                    <link rel="icon" href="/assets/SPK_LOGO.jpg" type="image/x-icon">
-                    <meta name="description" content="BlackMarket - Your go-to platform for making informed investment decisions. Explore comprehensive guides, tools, and resources to navigate financial markets with ease.">
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <link rel="stylesheet" href="/dist/output.css">
-                    <link rel="stylesheet" href="/dist/input.css">
-                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.css">
-                    <link rel="preconnect" href="https://fonts.googleapis.com">
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-                    <title>Start investing and find the best application for you on this website. My best application is ${bestApp}. Check it out and find the app that suits your needs!</title>
-                    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-                    <script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=669a9699cc04fc0019b7640e&product=inline-share-buttons&source=platform" async="async"></script>
-                    <style>
-                        body {
-                            opacity: 0;
-                            animation: fadeIn 2s forwards;
-                        }
-                        @keyframes fadeIn {
-                            to {
-                                opacity: 1;
-                            }
-                        }
-                    </style>
-                </head>
-                <body class="bg-[#f7f7f7] max-w-screen-lg mx-auto px-4 py-4 sm:px-6 md:px-8 lg:px-10" style="font-family: 'Poppins', sans-serif;">
-                    <nav class="max-w-screen-lg mx-auto bg-pink-500 rounded-lg m-4">
-                        <div class="flex flex-wrap items-center justify-between mx-auto p-4">
-                            <a href="#" class="flex items-center space-x-2">
-                                <img src="/assets/SPK_LOGO.jpg" class="h-8" alt="Logo">
-                                <span class="text-md lg:text-xl font-bold text-white"><span class="text-black">Black</span>Market</span>
-                            </a>
-                            <div class="flex space-x-1 rtl:space-x-reverse">
-                                <a href="/index.html">
-                                    <button type="button" class="shadow-button hover:text-pink-500 rounded-lg text-black bg-white transition-transform duration-200 ease-in-out transform hover:translate-y-1 focus:ring-white focus:outline-none focus:border-transparent font-medium text-sm px-4 py-2 text-center">Go to Home</button>
-                                </a>
-                            </div>
-                        </div>
-                    </nav>
-                    <div class="mb-8 text-center">
-                        <h2 class="text-4xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-black capitalize">Start Your <span class="text-pink-500">Investment</span> Journey with <span class="text-pink-500">${bestApp}</span></h2>
-                        <p class="mt-4 font-light text-gray-500">Your best application is <strong class="font-semibold text-black">${bestApp}</strong> based on the selected criteria.</p>
-                        <p class="font-light text-gray-500">The second best option is <strong class="font-semibold text-black">${secondBestApp}</strong>.</p>
-                        <p class="mt-4 font-normal italic">${reasons[bestApp]}</p>
-                    </div>
-                    <img src="/assets/${bestApp}.png" alt="${bestApp}" class="rounded-lg shadow-button mb-4 w-full mx-auto">
-                    <div class="mt-8 overflow-x-auto">
-                        <h3 class="text-2xl font-bold mb-4">SAW Table</h3>
-                        <div class="inline-block min-w-full rounded-lg overflow-hidden">
-                           <table class="min-w-full bg-white">
-                                 <thead class="bg-gray-100">
-                                    <tr>
-                                       <th class="sticky left-0 bg-gray-100 py-2 px-4 border-b">Criteria</th>
-                                       ${Object.keys(totalScores).map(app => `<th class="py-2 px-4 border-b">${app}</th>`).join('')}
-                                    </tr>
-                                 </thead>
-                                 <tbody class="text-gray-500">
-                                    ${questions.map((question, index) => `
-                                       <tr>
-                                             <td class="sticky left-0 bg-white py-2 px-4 border-b">${question.question}</td>
-                                             ${Object.keys(totalScores).map(app => `<td class="py-2 px-4 border-b">${(question.scores[app] * userWeights[index]).toFixed(2)}</td>`).join('')}
-                                       </tr>
-                                    `).join('')}
-                                 </tbody>
-                           </table>
-                        </div>
-                     </div>
-                    <div class="mt-8">
-                        <h3 class="text-2xl font-bold mb-4">Normalized Scores</h3>
-                        <table class="min-w-full bg-white rounded-lg overflow-hidden">
-                            <thead class="bg-gray-100">
-                                <tr>
-                                    <th class="py-2 px-4 border-b">Application</th>
-                                    <th class="py-2 px-4 border-b">Normalized Score</th>
-                                </tr>
-                            </thead>
-                            <tbody class="text-gray-500">
-                                ${sortedResults.map(([app, score]) => `
-                                    <tr>
-                                        <td class="py-2 px-4 border-b">${app}</td>
-                                        <td class="py-2 px-4 border-b">${score}</td>
-                                    </tr>
-                                `).join('')}
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="mt-8 sharethis-inline-share-buttons"></div>
-                    <script src="/node_modules/flowbite/dist/flowbite.min.js"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
-                </body>
-                </html>
-            `);
+              <html>
+              <head>
+                  <link rel="icon" href="/assets/SPK_LOGO.jpg" type="image/x-icon">
+                  <meta name="description" content="BlackMarket - Your go-to platform for making informed investment decisions. Explore comprehensive guides, tools, and resources to navigate financial markets with ease.">
+                  <meta charset="UTF-8">
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                  <link rel="stylesheet" href="/dist/output.css">
+                  <link rel="stylesheet" href="/dist/input.css">
+                  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.css">
+                  <link rel="preconnect" href="https://fonts.googleapis.com">
+                  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+                  <title>Start investing and find the best application for you on this website. My best application is ${bestApp}. Check it out and find the app that suits your needs!</title>
+                  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+                  <script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=669a9699cc04fc0019b7640e&product=inline-share-buttons&source=platform" async="async"></script>
+                  <style>
+                      body {
+                          opacity: 0;
+                          animation: fadeIn 2s forwards;
+                      }
+                      @keyframes fadeIn {
+                          to {
+                              opacity: 1;
+                          }
+                      }
+                  </style>
+              </head>
+              <body class="bg-[#f7f7f7] max-w-screen-lg mx-auto px-4 py-4 sm:px-6 md:px-8 lg:px-10" style="font-family: 'Poppins', sans-serif;">
+                  <nav class="max-w-screen-lg mx-auto bg-pink-500 rounded-lg m-4">
+                      <div class="flex flex-wrap items-center justify-between mx-auto p-4">
+                          <a href="#" class="flex items-center space-x-2">
+                              <img src="/assets/SPK_LOGO.jpg" class="h-8" alt="Logo">
+                              <span class="text-md lg:text-xl font-bold text-white"><span class="text-black">Black</span>Market</span>
+                          </a>
+                          <div class="flex space-x-1 rtl:space-x-reverse">
+                              <a href="/index.html">
+                                  <button type="button" class="shadow-button hover:text-pink-500 rounded-lg text-black bg-white transition-transform duration-200 ease-in-out transform hover:translate-y-1 focus:ring-white focus:outline-none focus:border-transparent font-medium text-sm px-4 py-2 text-center">Go to Home</button>
+                              </a>
+                          </div>
+                      </div>
+                  </nav>
+                  <div class="mb-8 text-center">
+                      <h2 class="text-4xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-black capitalize">Start Your <span class="text-pink-500">Investment</span> Journey with <span class="text-pink-500">${bestApp}</span></h2>
+                      <p class="mt-4 font-light text-gray-500">Your best application is <strong class="font-semibold text-black">${bestApp}</strong> based on the selected criteria.</p>
+                      <p class="font-light text-gray-500">The second best option is <strong class="font-semibold text-black">${secondBestApp}</strong>.</p>
+                      <p class="mt-4 font-normal italic">${reasons[bestApp]}</p>
+                  </div>
+                  <img src="/assets/${bestApp}.png" alt="${bestApp}" class="rounded-lg shadow-button mb-4 w-full mx-auto">
+                  <div class="mt-8 overflow-x-auto">
+                      <h3 class="text-2xl font-bold mb-4">SAW Table</h3>
+                      <div class="inline-block min-w-full rounded-lg overflow-hidden">
+                          <table class="min-w-full bg-white">
+                              <thead class="bg-gray-100">
+                                  <tr>
+                                      <th class="sticky left-0 bg-gray-100 py-2 px-4 border-b">Criteria</th>
+                                      ${Object.keys(totalScores).map(app => `<th class="py-2 px-4 border-b">${app}</th>`).join('')}
+                                  </tr>
+                              </thead>
+                              <tbody class="text-gray-500">
+                                  ${questions.map((question, index) => `
+                                      <tr>
+                                          <td class="sticky left-0 bg-white py-2 px-4 border-b">${question.question}</td>
+                                          ${Object.keys(totalScores).map(app => {
+            const score = question.isCost ? ((question.scores[app] - maxScore) / maxScore) : question.scores[app];
+            return `<td class="py-2 px-4 border-b">${(score * userWeights[index]).toFixed(2)}</td>`;
+         }).join('')}
+                                      </tr>
+                                  `).join('')}
+                              </tbody>
+                          </table>
+                      </div>
+                  </div>
+                  <div class="mt-8">
+                      <h3 class="text-2xl font-bold mb-4">Normalized Scores</h3>
+                      <table class="min-w-full bg-white rounded-lg overflow-hidden">
+                          <thead class="bg-gray-100">
+                              <tr>
+                                  <th class="py-2 px-4 border-b">Application</th>
+                                  <th class="py-2 px-4 border-b">Normalized Score</th>
+                              </tr>
+                          </thead>
+                          <tbody class="text-gray-500">
+                              ${sortedResults.map(([app, score]) => `
+                                  <tr>
+                                      <td class="py-2 px-4 border-b">${app}</td>
+                                      <td class="py-2 px-4 border-b">${score}</td>
+                                  </tr>
+                              `).join('')}
+                          </tbody>
+                      </table>
+                  </div>
+                  <div class="mt-8 sharethis-inline-share-buttons"></div>
+                  <script src="/node_modules/flowbite/dist/flowbite.min.js"></script>
+                  <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
+              </body>
+              </html>
+          `);
          newWindow.document.close();
 
       } else {
          console.log("Tidak ada skor yang valid untuk normalisasi.");
       }
-
    }
+
 
    nextBtn.addEventListener('click', handleNext);
 
